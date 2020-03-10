39a40b66cb6a814a497f7073992db46a0f7a080b
xhh <xuhao@u51.com>
2020-03-10 13:43:52 +0800

    [MDF] podspec修改

--- flutterapp/FlutterProject.podspec
+++ flutterapp/FlutterProject.podspec
@@ -8,7 +8,8 @@ Pod::Spec.new do |s|
     s.author                = { 'Flutter Dev Team' => 'flutter-dev@googlegroups.com' }
     s.source                = { :git => 'https://github.com/X-HH/FlutterProject.git', :tag => s.version }
     s.ios.deployment_target = '8.0'
-    s.vendored_frameworks   = 'build_for_ios/*.framework'
-    s.source_files = 'build_for_ios/*.{h,m}'
+    s.vendored_frameworks   = 'flutterapp/build_for_ios/*.framework'
+    s.source_files = 'flutterapp/build_for_ios/*.{h,m}'
+    s.pod_target_xcconfig = { 'VALID_ARCHS[sdk=iphonesimulator*]' => '' }
   end
   
--- flutterapp/build_for_ios.sh
+++ flutterapp/build_for_ios.sh
@@ -9,20 +9,36 @@ echo "开始获取 packages 插件资源"
 flutter packages get
 
 echo "开始构建 release for ios"
-# flutter build ios --release 
+FLUTTER_BUILD_MODE=release
+flutter build ios --release 
 echo "构建 release 已完成"
+mkdir ./build/release
+cp -a ios/Flutter/App.framework ./build/release
 
 echo "开始构建 —simulator for ios"
+FLUTTER_BUILD_MODE=debug
 flutter build ios --simulator
 echo "构建 —simulator 已完成"
+mkdir ./build/debug
+cp -a ios/Flutter/App.framework ./build/debug
+
+mkdir ./build/des
+lipo -create ./build/debug/App.framework/App  ./build/release/App.framework/App -output ./build/des/App
+
+lipo -create ./build/ios/Debug-iphonesimulator/shared_preferences/shared_preferences.framework/shared_preferences  ./build/ios/Release-iphoneos/shared_preferences/shared_preferences.framework/shared_preferences -output ./build/des/shared_preferences
 
 echo "开始 处理framework和资源文件"
 mkdir build_for_ios
 
+cp -a ./build/debug/App.framework ./build/des
+# rm -rf ./build/des/App.framework/App
+cp -a ./build/des/App ./build/des/App.framework
 
-cp -r build/ios/Debug-iphonesimulator/*/*.framework build_for_ios
-cp -r ios/Flutter/App.framework build_for_ios
-cp -r ios/Flutter/Flutter.framework build_for_ios
+cp -a ./build/ios/Debug-iphonesimulator/shared_preferences/shared_preferences.framework ./build/des
+cp -a ./build/des/shared_preferences ./build/des/shared_preferences.framework
+# rm -rf ./build/des/App
+
+cp -r ./build/des/*.framework build_for_ios
 cp -r ios/Runner/GeneratedPluginRegistrant.* build_for_ios
 
 
